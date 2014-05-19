e6a6658ea19a8602d660b446116689d53674af0f
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-05-19 15:05:38 +1000

    added uid generation

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,5 +10,20 @@
 #
 
 class Group < ActiveRecord::Base
-  attr_accessible :name, :uid
+  attr_accessible :name
+  
+  #============================================
+  # Callbacks
+  #============================================
+  after_create :generate_uid
+  
+  private
+    # Intialize the UID and save the record
+    def generate_uid
+      if self.id && !self.uid
+        self.uid = "cld-#{self.id}"
+        Group.update_all({uid:self.uid}, {id: self.id})
+      end
+      return true
+    end
 end
