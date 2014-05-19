092002b7b976d6ba9d451a89b113e4a5dba98811
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-05-19 15:03:59 +1000

    scaffolded Group model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140518112153) do
+ActiveRecord::Schema.define(:version => 20140519050316) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.string   "uid"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
