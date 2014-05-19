2befd806312da6adc692b05a062376f605fa6236
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-05-19 15:08:32 +1000

    annotated

--- app/models/group_user_rel.rb
+++ app/models/group_user_rel.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: group_user_rels
+#
+#  id         :integer         not null, primary key
+#  group_id   :integer
+#  user_id    :integer
+#  role       :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 class GroupUserRel < ActiveRecord::Base
   attr_accessible :group_id, :role, :user_id
 end
--- spec/models/group_user_rel_spec.rb
+++ spec/models/group_user_rel_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: group_user_rels
+#
+#  id         :integer         not null, primary key
+#  group_id   :integer
+#  user_id    :integer
+#  role       :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 require 'spec_helper'
 
 describe GroupUserRel do
