42f9763aad4dc2b49fa4376c8090d98f11a3c29d
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-05-19 14:36:54 +1000

    migrated users and annotated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :integer         not null, primary key
+#  email             :string(255)
+#  name              :string(255)
+#  surname           :string(255)
+#  free_trial_end_at :datetime
+#  geo_country_code  :string(255)
+#  uid               :string(255)
+#  sso_session       :string(255)
+#  created_at        :datetime        not null
+#  updated_at        :datetime        not null
+#
+
 class User < ActiveRecord::Base
   attr_accessible :email, :free_trial_end_at, :geo_country_code, :name, :sso_session, :surname
   
