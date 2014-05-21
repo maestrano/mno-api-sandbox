4b5276f0260745e68975c7bdd9428b14bdc9c0f7
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-05-21 10:23:51 +1000

    added secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MnoApiSandbox::Application.config.secret_token = '1e06eb080c84c146325aa28f60fad15ce52482234d98eaeea10e9888789ae1d82fcb2645b96cbfbd732c85bdbc0cfe286d8a6793e0d8c44670c2dfedd5748b62'
