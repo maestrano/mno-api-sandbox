bb48d6177c636fca2beca4b8628f217d8704ff38
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-10-26 01:41:42 +1100

    added guarding

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -8,15 +8,15 @@ class LogsController < ApplicationController
   # GET /logs
   def index
     @logs = []
-    content = File.read("log/#{Rails.env}.log").encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+    if File.exist?("log/#{Rails.env}.log")
+      content = File.read("log/#{Rails.env}.log").encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
     
-    puts
-    
-    content.split(/\n{2,}/).each do |line|
-      next unless line =~ /\/api/i
+      content.split(/\n{2,}/).each do |line|
+        next unless line =~ /\/api/i
       
-      @logs.unshift(line)
+        @logs.unshift(line)
+      end
+      @logs = @logs[0..100]
     end
-    @logs = @logs[0..100]
   end
 end
