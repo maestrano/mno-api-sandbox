34bb61b26b40167c68330be881066d1d00eba504
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-10-26 01:52:54 +1100

    log attempts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ MnoApiSandbox::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
-  config.logger = Logger.new MultiIO.new(STDOUT, "tmp/production.log")
+  config.logger = Logger.new MultiIO.new(STDOUT, File.open("tmp/production.log", "a"))
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
