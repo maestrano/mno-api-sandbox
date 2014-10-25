751c7272ac217af497059531b7e2eab781dc90c6
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-10-26 03:19:57 +1100

    colorize

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -5,6 +5,7 @@ module LogsHelper
       .gsub("\n","<br/>")
       .gsub(/\[\d+m/,'')
       .gsub(/^(\s)+/,"&nbsp;&nbsp;nbsp;nbsp;nbsp;nbsp;")
+      .gsub(/(Started\s+\w+)/,"<span class='text-warning'><b>\\1</b></span>")
       .gsub("ERROR:","<br/><span class='text-error'><b>ERROR:&nbsp;</b></span>")
       .gsub("INSPECT:","<br/><span class='text-info'><b>INSPECT:&nbsp;</b></span>")
       .gsub(/(Completed\s+20\d\s+OK)/,"<br/><span class='text-success'><b>\\1</b></span>")
