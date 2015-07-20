f61dbdb3e6c545fb63ce0ac49c87e7cde7d0d08a
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-07-20 23:08:49 +1000

    filter filtering params

--- app/controllers/api/v1/account/reseller_groups_controller.rb
+++ app/controllers/api/v1/account/reseller_groups_controller.rb
@@ -4,8 +4,13 @@ class Api::V1::Account::ResellerGroupsController < Api::V1::Account::ResellersCo
   # Action completely mocked. Return all customer groups belonging
   # to the app provider
   def index
-    @entities = current_app.groups.with_param_filters(params)
+    @entities = current_app.groups.with_param_filters(filtering_params)
     logger.info("INSPECT: entities => #{@entities.to_json}")
     render 'api/v1/account/groups/index'
   end
+
+  private
+    def filtering_params
+      params.reject { |k,v| [:reseller_id].include?(k.to_sym) }
+    end
 end
